cee415cf416a195f4b5efe8e56e446ca867eb495
srcrapi <rafa1000.skylanders@gmail.com>
2024-07-17 16:23:39 +0100

    feat: add projects tab

---
+++ scripts/projects_background.js
@@ -0,0 +1,17 @@
+// Ordenado na ordem que ta no html
+const backgrounds = ["nijika-wallpaper.jpg", "ryo_image.jpg", "kita_image.jpg"];
+const basePath = "/assets/img/Projects/banners";
+
+const setBackground = () => {
+  let cards = document.querySelectorAll(".project-card");
+
+  cards.forEach((card, i) => {
+    if (!backgrounds[i]) console.log("Missing image for", card);
+
+    card.style.backgroundImage = `url(${basePath}/${backgrounds[i]})`;
+  });
+};
+
+document.addEventListener("DOMContentLoaded", () => {
+  setBackground();
+});
